eb30c8260b1e24235717ffb82f3879a26076d44f
willofD13 <leonidastsiakas13@gmail.com>
2025-02-09 17:50:35 +0200

    Create metrics routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  get "/metrics/new", to: "metrics#new", as: :new_metric
+  post "/metrics", to: "metrics#create", as: :metrics
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "metrics#index"
 end
