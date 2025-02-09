0c45579cda806fb7017904395bf3b80ffb104312
willofD13 <leonidastsiakas13@gmail.com>
2025-02-09 17:49:45 +0200

    Create controller actions

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,2 +1,23 @@
 class MetricsController < ApplicationController
+  def index
+    @metrics = Metrics.all
+  end
+
+  def new
+    @metric = Metrics.new
+  end
+
+  def create
+    @metric = Metrics.new(metrics_params)
+    if @metric.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def metrics_params
+      params.require(:metric).permit(:time, :distance)
+    end
 end
