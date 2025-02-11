3afa2676af01ce3ffb982df244fcba668897f5cd
willofD13 <leonidastsiakas13@gmail.com>
2025-02-11 21:48:53 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "/metrics/new", to: "metrics#new", as: :new_metric
-  post "/metrics", to: "metrics#create", as: :metrics
-  get "/metrics/:id", to: "metrics#show", as: :metric_path
-  get "/metrics/:id/edit", to: "metrics#edit", as: :edit_metric_path
-  patch "/metrics/:id", to: "metrics#update"
-  delete "metrics/:id", to: "metrics#destroy"
+  resources :metrics
   # Defines the root path route ("/")
   root "metrics#index"
 end
