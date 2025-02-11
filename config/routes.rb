848a5846961c631ffd0cb3a0a3b49f36c938f0ce
willofD13 <leonidastsiakas13@gmail.com>
2025-02-11 21:34:49 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "/metrics/new", to: "metrics#new", as: :new_metric
   post "/metrics", to: "metrics#create", as: :metrics
+  get "/metrics/:id", to: "metrics#show", as: :metric_path
+  get "/metrics/:id/edit", to: "metrics#edit", as: :edit_metric_path
+  patch "/metrics/:id", to: "metrics#update"
+  delete "metrics/:id", to: "metrics#destroy"
   # Defines the root path route ("/")
   root "metrics#index"
 end
