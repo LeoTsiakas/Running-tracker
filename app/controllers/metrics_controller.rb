af5d0d3731e4c607e48c53ac887795b38b62601d
willofD13 <leonidastsiakas13@gmail.com>
2025-02-11 21:25:47 +0200

    Add metrics edit feature

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -6,13 +6,26 @@ class MetricsController < ApplicationController
   def new
     @metric = Metrics.new
   end
+  
+  def edit
+    @metric = Metrics.find(params[:id])
+  end
+
+  def update
+    @metric = Metrics.find(params[:id])
+    if @metric.update(metrics_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def create
     @metric = Metrics.new(metrics_params)
     if @metric.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
