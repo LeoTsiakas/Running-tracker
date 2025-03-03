b5049297abd0bf715e3ca8d25e08f99d0bc8e5d0
willofD13 <leonidastsiakas13@gmail.com>
2025-03-03 21:03:45 +0200

    Add password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_16_133500) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_03_190129) do
   create_table "metrics", force: :cascade do |t|
     t.string "time"
     t.integer "distance"
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_16_133500) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -29,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_16_133500) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
