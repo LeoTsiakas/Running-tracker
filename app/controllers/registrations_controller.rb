53d6a2227cf8a316ebd0e2146d65427003bd8846
willofD13 <leonidastsiakas13@gmail.com>
2025-03-03 21:05:04 +0200

    Create user rregistration

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new
+    end
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
