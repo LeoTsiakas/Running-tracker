6a9bd3c50f6752d405994358cc5c0464c20d9481
willofD13 <leonidastsiakas13@gmail.com>
2025-03-16 21:50:49 +0200

    Implement log in funtionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+
+    if @user.present? && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Successfully logged in'
+    else
+      redirect_to sign_in_path, notice: 'Invalid email or password'
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'Successfully logged out'
