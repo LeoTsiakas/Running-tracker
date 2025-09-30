05359ed7967543f4f8d400f823c026ab3b355073
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2025-09-30 20:53:50 +0300

    Add validation for date not to be in the future

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,7 +1,14 @@
 class Metric < ApplicationRecord
   validates :time, :distance, :date, presence: true
+  validate :date_cannot_be_in_the_future
 
   belongs_to :user
 
   scope :ordered, -> { order(date: :asc) }
+
+  private
+
+  def date_cannot_be_in_the_future
+    errors.add(:date, "can't be in the future") if date.in_time_zone > Time.current
+  end
 end
