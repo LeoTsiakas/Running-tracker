c1ee2f3e24bc6efeeca8149e0da9dc943ba7ac79
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2025-10-13 22:49:43 +0300

    Validate metric date in user's timezone

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -10,6 +10,9 @@ class Metric < ApplicationRecord
   private
 
   def date_cannot_be_in_the_future
-    errors.add(:date, "can't be in the future") if date.in_time_zone > Time.current
+    Time.use_zone(Current.user.time_zone) do
+      local_date = Time.zone.local(date.year, date.month, date.day, date.hour, date.min, date.sec)
+      errors.add(:date, "can't be in the future") if local_date > Time.current
+    end
   end
 end
