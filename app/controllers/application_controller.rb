cc1f1c653782575692536dac169ed85703c322dc
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2026-08-24 19:31:26 +0300

    Implement guest layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:time_zone])
     devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone])
+  end
+
+  def layout_by_resource
+    user_signed_in? ? 'application' : 'guest'
   end
 end
