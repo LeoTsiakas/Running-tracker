141a2df2de06b74277629ee2cb463742e0e30ca7
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2026-09-19 14:39:56 +0300

    Add distance to date chart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def build_chart_data(metrics, attribute)
+    metrics.group_by_day { |metric| metric.date }.to_h do |k, metrics|
+      [k, metrics.map { |metric| metric.send(attribute) }.sum]
+    end
+  end
 end
